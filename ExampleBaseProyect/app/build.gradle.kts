bac88348db5c9013d5b4da73e88992269bf64b88
Antonio Villa <antonio.va2301@hotmail.com>
2026-07-27 17:11:03 -0600

    Connecting modules, cleaning manifest for dat module and deleting empty class in domain

--- ExampleBaseProyect/app/build.gradle.kts
+++ ExampleBaseProyect/app/build.gradle.kts
@@ -36,12 +36,14 @@ android {
 }
 
 dependencies {
+    implementation(project(":domain"))
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     testImplementation(libs.junit)
--- ExampleBaseProyect/data/build.gradle.kts
+++ ExampleBaseProyect/data/build.gradle.kts
@@ -21,10 +21,10 @@ android {
 }
 
 dependencies {
+    implementation(project(":domain"))
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.core.ktx)
     implementation(libs.material)
     testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(libs.androidx.junit)
 }
