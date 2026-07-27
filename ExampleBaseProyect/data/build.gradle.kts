9dcb8e0a07c0e3140da35c52958592d5025bdab9
Antonio Villa <antonio.va2301@hotmail.com>
2026-07-27 17:26:20 -0600

    Adding basic dependencies in data, room, retrofit

--- ExampleBaseProyect/data/build.gradle.kts
+++ ExampleBaseProyect/data/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.library)
+    id("com.google.devtools.ksp")
 }
 
 android {
@@ -27,4 +28,11 @@ dependencies {
     implementation(libs.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
+
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
+
+    implementation(libs.androidx.room.runtime)
+    ksp(libs.androidx.room.compiler)
+    implementation(libs.androidx.room.ktx)
 }
